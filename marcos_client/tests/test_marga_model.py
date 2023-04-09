89df75c69d32ecf9ecf1507ff4a409eb4a2e77ad
Max Stabel <M4a1x@users.noreply.github.com>
2023-04-09 08:10:57 +0200

    Fix base import

--- marcos_client/tests/test_marga_model.py
+++ marcos_client/tests/test_marga_model.py
@@ -10,7 +10,8 @@
 # To run a single test, use e.g.:
 # python -m unittest test_marga_model.Modeltest.test_many_quick
 
-from marcos_client.test_base import *
+from marcos_client.tests.test_base import *
+import unittest
 
 
 class ModelTest(unittest.TestCase):
